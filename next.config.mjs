9865b794ce7eefcedd779dc261a80296fdd64965
Florin Cojocariu <aathanor@gmail.com>
2024-09-18 14:21:28 +0300

    new template

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-// next.config.mjs
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  reactStrictMode: true,  // Your other Next.js configurations
-};
-
-export default nextConfig;
